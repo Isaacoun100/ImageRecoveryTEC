6d76f1098e636bd7a93415d23c836c2a80240ed1
Isaac Herrera Monge <Isaacoun100@gmail.com>
2021-11-04 16:21:34 -0600

    Testing code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "XML/ReadWrite.h"
+#include "image/ImageManager.h"
+#include "logic/HexGeneticAlgo.h"
 
 int main() {
-    ReadWrite readWrite;
-    cout<< readWrite.readFile("image/ti1.ppm")<<endl;
+    HexGeneticAlgo hexGeneticAlgo;
+    //ImageManager imageManager;
+    //ReadWrite readWrite;
+    //cout<< readWrite.readFile("image/ti1.ppm")<<endl;
+
     return 0;
 }
